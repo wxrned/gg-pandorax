2dce3dd783867ebfb05ccfd786e5b7ee66e303f2
warn <wxrn.lol@gmail.com>
2025-02-11 20:22:27 +0100

    Update events

---
+++ events/messageUpdate.js
@@ -0,0 +1,31 @@
+const config = require(`../data/config`);
+
+module.exports = {
+  name: "messageUpdate",
+  tier: null,
+  async execute(client, oldMessage, newMessage) {
+    if (
+      !oldMessage.guild ||
+      !oldMessage.channel ||
+      oldMessage.author.bot ||
+      oldMessage.content === newMessage.content
+    )
+      return;
+
+    if (!client.editsnipes) client.editsnipes = new Map();
+
+    const channelEdits = client.editsnipes.get(oldMessage.channel.id) || [];
+
+    channelEdits.unshift({
+      oldContent: oldMessage.content || "[No Content]",
+      newContent: newMessage.content || "[No Content]",
+      author: oldMessage.author.tag,
+      authorId: oldMessage.author.id,
+      timestamp: newMessage.editedTimestamp || Date.now(),
+    });
+
+    if (channelEdits.length > 25) channelEdits.pop();
+
+    client.editsnipes.set(oldMessage.channel.id, channelEdits);
+  },
+};
