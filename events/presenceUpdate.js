ad53f4c4df630c399d584da3c75487edeb2c7497
warn <wxrn.lol@gmail.com>
2025-02-11 20:44:48 +0100

    Update presenceUpdate.js

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -10,18 +10,15 @@ module.exports = {
     const targetRoleId = "1297968483123855431";
     const vanityCode = "/pandorax";
 
-    // Check if the user has a custom status
     const customStatus = newPresence.activities.find(
       (activity) => activity.type === 4 && activity.state
     );
 
     if (customStatus && customStatus.state.includes(vanityCode)) {
-      // Add role if the user is promoting the vanity invite
       if (!newPresence.member.roles.cache.has(targetRoleId)) {
         await newPresence.member.roles.add(targetRoleId).catch(console.error);
       }
     } else {
-      // Remove role if the user is no longer promoting the invite
       if (newPresence.member.roles.cache.has(targetRoleId)) {
         await newPresence.member.roles.remove(targetRoleId).catch(console.error);
       }
